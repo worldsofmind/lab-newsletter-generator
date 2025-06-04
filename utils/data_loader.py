3e2ca7bb57a5f5fd2b3269d293599e97a7948fee
worldsofmind <110438042+worldsofmind@users.noreply.github.com>
2025-06-04 09:54:09 +0800

    Add files via upload

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -24,7 +24,7 @@ def load_all_data(ratings_file, caseload_file, namelist_file):
     namelist_df = pd.read_csv(namelist_file, encoding=namelist_encoding)
     caseload_df = detect_header_and_load_excel(caseload_file)
 
-    date_col = next((col for col in caseload_df.columns if 'Date' in col and 'Assigned' in col), None)
+    date_col = next((col for col in caseload_df.columns if 'Date' in str(col) and 'Assigned' in str(col)), None)
     if date_col is None:
         raise ValueError("Could not identify the 'Date Assigned' column in case_load.")
 
