19fcd1c6375c7379e319d7078a9174ec42c316d2
worldsofmind <110438042+worldsofmind@users.noreply.github.com>
2025-06-04 17:14:48 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,39 +1,40 @@
+
 import streamlit as st
 import pandas as pd
 from utils.data_loader import load_all_data
-from utils.processor import process_all_officers, compute_officer_stats
-from utils.render import render_newsletters
+from utils.processor import process_all_officers
+from utils.renderer import render_newsletters
 
-st.set_page_config(page_title="LAB Officer Newsletter Generator", layout="centered")
+st.set_page_config(page_title="LAB Newsletter Generator", layout="centered")
 st.title("📬 LAB Officer Newsletter Generator")
 
-# Sidebar for uploads
-st.sidebar.header("📂 Upload Files")
-ratings_file = st.sidebar.file_uploader("Upload ratings.csv", type="csv")
-caseload_file = st.sidebar.file_uploader("Upload case_load.xlsx", type=["xls", "xlsx"])
-namelist_file = st.sidebar.file_uploader("Upload namelist.csv", type="csv")
+with st.sidebar:
+    st.header("📂 Upload Files")
+    ratings_file = st.file_uploader("Upload `ratings.csv`", type="csv")
+    caseload_file = st.file_uploader("Upload `case_load.xlsx`", type=["xls", "xlsx"])
+    namelist_file = st.file_uploader("Upload `namelist.csv`", type="csv")
 
-# Checkbox for batch or individual
-generate_all = st.sidebar.checkbox("🔁 Generate for All Officers", value=True)
-selected_officer = None
-
-if not generate_all:
-    selected_officer = st.sidebar.selectbox("👤 Select Officer", ["-- Select --"])
-
-if ratings_file and caseload_file and namelist_file:
-    with st.spinner("Processing data..."):
-        ratings_df, caseload_df, namelist_df, period = load_all_data(ratings_file, caseload_file, namelist_file)
-
-    if generate_all:
-        officer_reports = process_all_officers(ratings_df, caseload_df, namelist_df, period)
-    elif selected_officer != "-- Select --":
-        officer_row = namelist_df[namelist_df["name_self"] == selected_officer].iloc[0]
-        officer_reports = [compute_officer_stats(officer_row, caseload_df, ratings_df, period)]
-    else:
-        st.warning("Please select an officer or choose to generate all.")
-        st.stop()
-
-    render_newsletters(officer_reports, period)
-    st.success("Newsletters generated successfully. Check the 'generated' folder.")
+if not ratings_file or not caseload_file or not namelist_file:
+    st.error("Please upload all required files.")
 else:
-    st.info("Please upload all required files.")
+    with st.spinner("Processing data and generating newsletters..."):
+        ratings_df, caseload_df, namelist_df, period = load_all_data(
+            ratings_file, caseload_file, namelist_file
+        )
+
+        officer_names = ["-- Select --"] + namelist_df['name_self'].dropna().unique().tolist()
+        selected_officer = st.selectbox("Select Officer to Generate Newsletter For", officer_names)
+        generate_all = st.checkbox("Generate Newsletters for All Officers")
+
+        if st.button("Generate Newsletter"):
+            if generate_all:
+                officer_reports = process_all_officers(ratings_df, caseload_df, namelist_df, period)
+            elif selected_officer != "-- Select --":
+                officer_row = namelist_df[namelist_df['name_self'] == selected_officer].iloc[0]
+                officer_reports = [compute_officer_stats(officer_row, caseload_df, ratings_df, period)]
+            else:
+                st.warning("Please select an officer or choose to generate for all.")
+                st.stop()
+
+            render_newsletters(officer_reports, period)
+            st.success("Newsletters generated successfully. Check the 'generated' folder.")
